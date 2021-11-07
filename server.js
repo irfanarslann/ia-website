8f476eaee01d068deebce25c8f3118d69c9da476
samSepi0l <irfanarslaann@gmail.com>
2021-11-07 15:47:27 +0300

    Initial Commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log("Server Connected"));
